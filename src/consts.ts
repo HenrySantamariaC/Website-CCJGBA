20679347adc9b566fd2981a7363ba1b8df415edd
Henry Santamaria C <hsantamariaca@unprg.edu.pe>
2023-05-21 12:47:59 -0500

    Seccion Testimonios finalizada - Landing CCJGBA

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Casa Comunal de la Juventud Guillermo Baca Aguinaga';
+export const SITE_DESCRIPTION = 'La Casa Comunal de la Juventud brinda servicios de calidad y accesibles. Aquí podrás realizar deportes, eventos, clases y otras actividades.';
